c38494b66d23cc7b6270045a39ba7f2db0772528
Selim <sloumma.21@gmail.com>
2018-10-14 19:46:06 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjoet-dev', {
+        autoIndex: false,
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log('MongoDB Connected');
+    }).catch(err => console.log(err));
+
+
+
+
+//HandleBars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body Parser middleWare
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Method Override middleware
+app.use(methodOverride('_method'));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+
+// Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
+
+app.get('/', (req, res) => {
+    const title = 'Welcome'
+    res.render('index', {
+        title: title
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
